574fa206e5f73b5f64f8b529c8ca7a71399432c5
MrADS2o3 <aridwis203@gmail.com>
2023-03-17 11:00:48 +0700

    Update test suite

--- Test Suites/Produk/Tambah dan Hapus produk dari keranjang.ts
+++ Test Suites/Produk/Tambah dan Hapus produk dari keranjang.ts
@@ -11,4 +11,18 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>70f0c259-6ad7-4821-90f1-f67d55038480</testSuiteGuid>
+   <testCaseLink>
+      <guid>63a53a1e-b358-4cba-8823-2331e25ee48b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PROD - Produk/PKJ - Produk di keranjang/PKJ01_P_Tambah produk ke keranjang</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f0bd9b91-edf5-48b1-8649-000a93cb4ced</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PROD - Produk/PKJ - Produk di keranjang/PKJ02_N_Hapus produk dari keranjang</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
